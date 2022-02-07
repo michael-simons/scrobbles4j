e66e11c74a5862fec5893d03e20f650b1cc1de14
Michael Simons <michael@simons.ac>
2022-02-07 10:17:48 +0100

    Integration tests are not 'Only for the weak'.

---
+++ server/src/test/resources/db/migration/common/afterMigrate.sql
@@ -0,0 +1,6 @@
+INSERT INTO artists VALUES (1, 'In Flames');
+INSERT INTO genres VALUES (1, 'Death Metal');
+INSERT INTO tracks VALUES (1, 1, 1, 'Clayman', 'Bulleted Ride', 2000, 312, 100, 'A comment', 1, 1, 1, 12, 'f');
+INSERT INTO tracks VALUES (2, 1, 1, 'Clayman', 'Only For The Weak', 2000, 295, 100, 'A comment', 1, 1, 3, 12, 'f');
+INSERT INTO plays (track_id, played_on) VALUES(1, curdate() - INTERVAL 1 DAY);
+INSERT INTO plays (track_id, played_on) VALUES(2, curdate());
